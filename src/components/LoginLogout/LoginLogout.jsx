93623e14cc3aca58e9ead602f1581a06115bdf7e
Lautaro Montaña <joselautarom@gmail.com>
2023-05-06 18:33:28 -0300

    Se valida, si el usuario está logueado, desaparece el botón ir al form, si no, sigue ahí

--- src/components/LoginLogout/LoginLogout.jsx
+++ src/components/LoginLogout/LoginLogout.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import  {LoginWithGoogle}  from './LoginWithGoogle/LoginWithGoogle'
-import { Link } from 'react-router-dom'
-// import { LoginWithForm } from './LoginWithForm/LoginWithForm'
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { LoginWithGoogle } from './LoginWithGoogle/LoginWithGoogle';
+import { Link } from 'react-router-dom';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const auth = firebase.auth();
 
 export const LoginLogout = () => {
+  const [user] = useAuthState(auth);
+
   return (
     <>
-        <div className="container">
-            
-        <LoginWithGoogle/>
-        <button className='container btn btn-info mt-2'>
-            <Link to='/loginwithform'>Ingresar con email</Link> 
-        </button>        
-        </div>
+      <div className="container">
+        <LoginWithGoogle />
+        {!user && (
+          <button className='container btn btn-info mt-2'>
+            <Link to='/loginwithform'>Ingresar con email</Link>
+          </button>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
+
