6f4cad57db179dc6707c6083cb755fae8595f460
Lautaro Montaña <joselautarom@gmail.com>
2023-05-08 19:25:03 -0300

    [Feature] Agregamos: horario y fecha de publicación, boton de Me Gusta con contador incluido

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,10 +1,11 @@
-import { addDoc, collection, getFirestore, getDocs, onSnapshot, deleteDoc, doc, setDoc } from "firebase/firestore"
-import React, { useState, useEffect, useContext } from "react"
+import { addDoc, collection, getFirestore, getDocs, onSnapshot, deleteDoc, doc, setDoc, serverTimestamp, updateDoc } from "firebase/firestore"
+import React, { useState, useEffect, useRef} from "react"
 import { AiFillEdit } from 'react-icons/ai'
 import { SlTrash } from 'react-icons/sl'
-import { GrEdit } from 'react-icons/gr'
+import { AiOutlineLike } from 'react-icons/ai'
 import { db } from '../../firebase'
 import Swal from "sweetalert2"
+import './Post.css'
 
 
 export const Post = () => {
@@ -17,60 +18,42 @@ export const Post = () => {
 
     const [description, setDescription] = useState("")
 
-    const [currentId, setCurrentId] = useState('')
+    // const user = auth.currentUser;
 
+    // const [currentId, setCurrentId] = useState('')
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("URL: ", url)
-        console.log("Name: ", name)
-        console.log("Description ", description)
+        e.preventDefault();
+        const db = getFirestore();
 
         const post = {
-            dates: {
-                url,
-                name,
-                description
-            }
-            // data: firebase.firestore.Timestamp.fromDate(new Date())
-        }
-
-        const db = getFirestore()
-
-        const newCollectionRef = collection(db, 'posts');
-        addDoc(newCollectionRef, {
             url: url,
             name: name,
-            description: description
-        })
+            description: description,
+            date: serverTimestamp(),// agregar la fecha del servidor
+            likes: 0,
+            // username: user.displayName
+        };
+
+        const newCollectionRef = collection(db, 'posts');
+        addDoc(newCollectionRef, post)
             .then((docRef) => {
                 Swal.fire({
                     icon: 'success',
                     showConfirmButton: false,
                     timer: 700
-                })
-                console.log('Document  created with id: ', docRef.id);
-                setDescription('')
-                setName('')
-                setUrl('')
+                });
+                console.log('Document created with id: ', docRef.id);
+                setDescription('');
+                setName('');
+                setUrl('');
             })
             .catch((error) => {
-                console.error('Error to create id: ', error);
+                console.error('Error creating document: ', error);
             });
+    };
 
-
-        postRef.update({
-            name: name,
-            description: description,
-        })
-            .then(() => {
-                console.log("Document successfully updated!");
-            })
-            .catch((error) => {
-                console.error("Error updating document: ", error);
-            });
-    }
-
+    // Funcion para obtener los posts y renderizarlos de nuevo con useEffect
     const getPosts = () => {
         const postsRef = collection(db, "posts");
         const unsubcribe = onSnapshot(postsRef, (querySnapshot) => {
@@ -79,7 +62,7 @@ export const Post = () => {
                 posts.push({ ...doc.data(), id: doc.id });
             });
             setPosts(posts)
-            console.log(posts)
+            // console.log(posts)
         });
         return () => unsubcribe()
     };
@@ -89,10 +72,10 @@ export const Post = () => {
     }, []);
 
 
-
+    // Funcion para eliminar los posts
     const handleDelete = (id) => {
         try {
-             deleteDoc(doc(db, "posts", id));
+            deleteDoc(doc(db, "posts", id));
             Swal.fire({
                 title: 'Tu mensaje se ha eliminado',
                 showConfirmButton: false,
@@ -103,76 +86,90 @@ export const Post = () => {
         }
     };
 
-    // const handleEdit = (post, newValues, db) => {
-    //     const postRef = doc(db, "posts", post.id);
-    //     const updatedPost = { ...post, ...newValues };
-    //     setDoc(postRef, updatedPost)
-    //       .then(() => console.log("Document updated!"))
-    //       .catch((error) => console.error("Error updating document: ", error));
-    //   }
 
 
+    // Funcion para darle like a una publicación
+    const handleLike = (postId, currentLikes) => {
+        const postRef = doc(db, "posts", postId);
+        updateDoc(postRef, { likes: currentLikes + 1 })
+            .then(() => {
+                console.log("Like updated successfully");
+                const updatedPosts = [...posts];
+                const index = updatedPosts.findIndex(post => post.id === postId); // encontrar el índice del post que se está actualizando
+                updatedPosts[index].likes = currentLikes + 1;
+                setPosts(updatedPosts);
+            })
+            .catch((error) => {
+                console.error("Error updating like: ", error);
+            });
+    };
+
+
+// Lista de post
 
 
     return (
-        
+
         <>
-        <div className="container">
-                <div className=" card text-center mb-3">
-                    <div className="card-body">
-
-
+            <div className="container">
+                    
                         {posts.map((post) => (
-                            <div className="card mb-1" key={post.id} >
+                            <div className="card mb-1" key={post.id}>
                                 <div className="d-flex justify-content-end">
                                     {/* <GrEdit className="m-3" onClick={() => handle(post.id)} /> */}
                                     <SlTrash className="m-3" onClick={() => handleDelete(post.id)} />
                                 </div>
-                                <div className="card-body">
-
+                                <div className="card-body text-center">
                                     <h4>{post.name}</h4>
                                     <p>{post.description}</p>
-                                    <a href={post.url} target="_blanck" rel="noopener noreferrer" className="btn btn-info col-12">Ir al enlace</a>
+                                    <button className="btn btn-primary" onClick={() => handleLike(post.id, post.likes)}><AiOutlineLike /></button>
+
+                                    <p>{post.likes} personas les gusta esto</p>
+                                    <p>
+                                        Publicado el {post.date ? new Date(post.date.seconds * 1000).toLocaleDateString("es-ES") : ''} a las{" "}
+                                        {post.date ? new Date(post.date.seconds * 1000).toLocaleTimeString("es-ES") : ''}
+                                    </p>
                                 </div>
                             </div>
-
                         ))}
                     </div>
-                </div>
-            </div>
             <hr />
-            <form onSubmit={handleSubmit} className="container mt-3">
-            <button className="col-12 btn btn-success">Publicar</button>
+            <div style={{position: 'fixed', bottom: '0', left: '0', width: '100%'}}>
+            <form onSubmit={handleSubmit} className="form-container container mt-3">
+                <button className="col-12 btn btn-success">Publicar</button>
                 <div className="card text-center mb-3" >
                     <div className="card-body">
-                        <h5 className="card-title">Agregar mensaje</h5>
+                        <h5 className="card-title">¿Como te sientes el dia de hoy?</h5>
                         <div className="input-group input-group-sm mb-3">
                             <span className="input-group-text" id="inputGroup-sizing-sm"><AiFillEdit /></span>
-                            <input type="text" className="form-control" placeholder="si desea, escriba su url"
-                                onChange={(e) => setUrl(e.target.value)}
-                                value={url}  
-                            />
-                        </div>
-
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm"><AiFillEdit /></span>
-                            <input type="text" className="form-control" placeholder="ingrese asunto del mensaje"
-                                onChange={(e) => setName(e.target.value)}
-                                value={name}
-                            />
-                        </div>
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm"><AiFillEdit /></span>
-                            <input type="text" className="form-control" placeholder="escriba aquí su mensaje"
+                            <input type="text" className="form-control" placeholder="escriba aquí su estado"
                                 onChange={(e) => setDescription(e.target.value)}
                                 value={description}
                             />
                         </div>
                     </div>
                 </div>
-                
+
             </form>
+            </div>
             <hr />
         </>
     )
 }
+{/* <div className="input-group input-group-sm mb-3">
+    <span className="input-group-text" id="inputGroup-sizing-sm"><AiFillEdit /></span>
+    <input type="text" className="form-control" placeholder="si desea, escriba su url"
+        onChange={(e) => setUrl(e.target.value)}
+        value={url}
+    />
+</div> */}
+
+{/* <div className="input-group input-group-sm mb-3">
+    <span className="input-group-text" id="inputGroup-sizing-sm"><AiFillEdit /></span>
+    <input type="text" className="form-control" placeholder="ingrese asunto del mensaje"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+    />
+</div> */}
+
+{/* <a href={post.url} target="_blanck" rel="noopener noreferrer" className="btn btn-info col-12">Ir al enlace</a> */ }
